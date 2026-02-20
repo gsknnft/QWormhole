110c69ac57541c7d51ba1489a6b91670c1fbdc5a
gsknnft <gsknnft@gmail.com>
2026-02-20 13:29:37 -0500

    fix to deep import types

--- src/coherence/qvariants.ts
+++ src/coherence/qvariants.ts
@@ -1,15 +1,17 @@
 import { QuantumSignalSuite } from "@sigilnet/qfield";
-import { QuantumFieldState, SignalVector } from "../../../../src/types/types";
+
+type SignalVector = Record<string, unknown>;
+type QuantumFieldState = Record<string, unknown>;
 
     export async function processAndLogSignal(signal: number[]): Promise<SignalVector> {
       const qss = new QuantumSignalSuite();
       const {signalAnalysis: vec} = await qss.processAndLog(signal);
-      return vec;
+      return vec as SignalVector;
     }
     export async function sigAnalysis(signal: number[]): Promise<{ signalAnalysis: SignalVector, qfield: QuantumFieldState, imfs: any, hilbertData: any, entropy: number }> {
       const {imfs, hilbertData, entropy} =  QuantumSignalSuite.runFullFieldAnalysis(Float64Array.from(signal));
       const suite = new QuantumSignalSuite();
       const {signalAnalysis} = await suite.processAndLog(signal);
-      const qfield = QuantumSignalSuite.evaluateSignalVector(imfs[0]);
-      return {signalAnalysis, qfield, imfs, hilbertData, entropy};
+      const qfield = QuantumSignalSuite.evaluateSignalVector(imfs[0]) as QuantumFieldState;
+      return {signalAnalysis: signalAnalysis as SignalVector, qfield, imfs, hilbertData, entropy};
     }
